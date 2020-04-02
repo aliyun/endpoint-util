5a935a29189433d4a7be8c005b4370a8a12c9f90
wenzuochao <wb-wzc505509@alibaba-inc.com>
2020-04-02 10:28:48 +0800

    add suffix

--- golang/service/service.go
+++ golang/service/service.go
@@ -10,21 +10,26 @@ import (
 	"strings"
 )
 
-func GetEndpointRules(product string, regionId string, endpointType string, network string) (_result string, _err error) {
+func GetEndpointRules(product string, regionId string, endpointType string, network string, suffix string) (_result string, _err error) {
 	if endpointType == "regional" {
 		if regionId == "" {
 			_err = fmt.Errorf("RegionId is empty, please set a valid RegionId")
 			return "", _err
 		}
-		_result = strings.Replace("<product><network>.<region_id>.aliyuncs.com", "<region_id>", regionId, 1)
+		_result = strings.Replace("<product><suffix><network>.<region_id>.aliyuncs.com", "<region_id>", regionId, 1)
 	} else {
-		_result = "<product><network>.aliyuncs.com"
+		_result = "<product><suffix><network>.aliyuncs.com"
 	}
 	_result = strings.Replace(_result, "<product>", strings.ToLower(product), 1)
 	if network == "" || network == "public" {
 		_result = strings.Replace(_result, "<network>", "", 1)
 	} else {
 		_result = strings.Replace(_result, "<network>", "-"+network, 1)
+	}
+	if suffix == "" {
+		_result = strings.Replace(_result, "<suffix>", "", 1)
+	} else {
+		_result = strings.Replace(_result, "<suffix>", "-"+suffix, 1)
 	}
 	return _result, nil
 }
--- golang/service/service_test.go
+++ golang/service/service_test.go
@@ -7,15 +7,15 @@ import (
 )
 
 func Test_GetEndpointRules(t *testing.T) {
-	endpoint, err := GetEndpointRules("ecs", "", "regional", "")
+	endpoint, err := GetEndpointRules("ecs", "", "regional", "", "")
 	utils.AssertEqual(t, "", endpoint)
 	utils.AssertEqual(t, "RegionId is empty, please set a valid RegionId", err.Error())
 
-	endpoint, err = GetEndpointRules("ecs", "cn-hangzhou", "regional", "")
+	endpoint, err = GetEndpointRules("ecs", "cn-hangzhou", "regional", "", "")
 	utils.AssertNil(t, err)
 	utils.AssertEqual(t, "ecs.cn-hangzhou.aliyuncs.com", endpoint)
 
-	endpoint, err = GetEndpointRules("ecs", "cn-hangzhou", "central", "intl")
+	endpoint, err = GetEndpointRules("ecs", "cn-hangzhou", "central", "intl", "test")
 	utils.AssertNil(t, err)
-	utils.AssertEqual(t, "ecs-intl.aliyuncs.com", endpoint)
+	utils.AssertEqual(t, "ecs-test-intl.aliyuncs.com", endpoint)
 }
