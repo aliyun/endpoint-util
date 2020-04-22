1c2f9a8573233a7e9f729dbac65ad0368825c1af
824750130 <e824750130@qq.com>
2020-04-22 13:23:15 +0800

    fix： modify code style

--- python/endpoint_util/client.py
+++ python/endpoint_util/client.py
@@ -1,26 +1,21 @@
-
 from Tea.exceptions import RequiredArgumentException
 
 
-class Client:
-    @staticmethod
-    def get_endpoint_rules(product, regionId, endpointType, network):
-        result = ""
-        product = product or ""
-        network = network or ""
-        if endpointType == "regional":
-            if regionId is None or regionId == "":
-                raise RequiredArgumentException(
-                    "RegionId is empty, please set a valid RegionId")
-            result = "<product><network>.<region_id>.aliyuncs.com".replace(
-                "<region_id>", regionId)
-        else:
-            result = "<product><network>.aliyuncs.com"
+def get_endpoint_rules(product, region_id, endpoint_type, network):
+    product = product or ""
+    network = network or ""
+    if endpoint_type == "regional":
+        if region_id is None or region_id == "":
+            raise RequiredArgumentException(
+                "RegionId is empty, please set a valid RegionId")
+        result = "<product><network>.<region_id>.aliyuncs.com".replace(
+            "<region_id>", region_id)
+    else:
+        result = "<product><network>.aliyuncs.com"
 
-        result = result.replace("<product>", product.lower())
-        if network == "" or network == "public":
-            result = result.replace("<network>", "")
-        else:
-            result = result.replace("<network>", "-"+network)
-
-        return result
+    result = result.replace("<product>", product.lower())
+    if network == "" or network == "public":
+        result = result.replace("<network>", "")
+    else:
+        result = result.replace("<network>", "-"+network)
+    return result
